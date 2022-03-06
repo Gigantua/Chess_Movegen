d561b2868cf890150991391812207eeb4b5c8f35
Daniel Inführ <di@instareconstruct.com>
2022-03-06 23:49:46 +0100

    Added bitreverse lookup

--- Bitrotation.hpp
+++ Bitrotation.hpp
@@ -1,6 +1,10 @@
 #pragma once
-
 #include <stdint.h>
+
+//(c) Gerd Isenberg, Aleks Peshkov 2007
+//Subtracting a Rook from a Blocking Piece - https://www.chessprogramming.org/Subtracting_a_Rook_from_a_Blocking_Piece
+
+//Improvement by Daniel Inführ 2022 - Using a non yet existing bitreverse intrinsics 
 
 namespace Chess_Lookup::Bitrotation
 {
@@ -10,7 +14,7 @@ namespace Chess_Lookup::Bitrotation
         return p ^ q ^ (q << k);
     }
 
-    uint64_t bitreverse(uint64_t n)
+    uint64_t bit_reverse(uint64_t n)
     {
         static const uint64_t  m1 = ((uint64_t(1) << 63) - 1) / (1 + (1 << 1) + (1 << 2));
         static const uint64_t  m2 = ((uint64_t(1) << 63) - 1) / (1 + (1 << 3) + (1 << 6));
@@ -25,11 +29,32 @@ namespace Chess_Lookup::Bitrotation
     }
     constexpr auto Size = 0; //0 kb version
 
-
-    static uint64_t Queen(int square, uint64_t occupation) {
-        uint64_t reversed = bitreverse(2); //1ull << 62
-
-        //Help here
-        return 42;
+    template<uint64_t bb>
+    uint64_t mask_shift(int ranks) {
+        return ranks > 0 ? bb >> (ranks << 3) : bb << -(ranks << 3);
     }
+
+#	define dir_HO(X) (0xFFull << (X & 56))
+#	define dir_VE(X) (0x0101010101010101ull << (X & 7))
+#	define dir_D1(X) (mask_shift<0x8040201008040201ull>((X & 7) - (X >> 3)))
+#	define dir_D2(X) (mask_shift<0x0102040810204080ull>(7 - (X & 7) - (X >> 3)))
+		
+	/* Generate attack using the hyperbola quintessence approach */
+	uint64_t attack(uint64_t pieces, uint32_t x, uint64_t mask) {
+		uint64_t o = pieces & mask;
+		return ((o - (1ull << x)) ^ bit_reverse(bit_reverse(o) - (1ull << (x ^ 63)))) & mask;
+	}
+
+    static uint64_t Queen(int sq, uint64_t occ) {
+
+        return	  attack(occ, sq, dir_HO(sq) ^ (1ull << sq))
+				| attack(occ, sq, dir_VE(sq) ^ (1ull << sq))
+				| attack(occ, sq, dir_D1(sq) ^ (1ull << sq))
+				| attack(occ, sq, dir_D2(sq) ^ (1ull << sq));
+    }
+
+#undef dir_HO
+#undef dir_VE
+#undef dir_D1
+#undef dir_D2
 }
